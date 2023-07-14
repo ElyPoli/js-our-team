d36f924cbf9ea3d12bb2f5056027154048b2e876
ElyPoli <itsos.poli@gmail.com>
2023-07-14 15:58:09 +0200

    bonus 1

--- js/main.js
+++ js/main.js
@@ -23,46 +23,58 @@ const boxTeam = document.querySelector("#box-team");
 
 const membersOfTeam = [
     {
-        nome: "Wayne Barnett",
-        ruolo: "Founder & CEO",
-        foto: "wayne-barnett-founder-ceo.jpg",
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        photo: "wayne-barnett-founder-ceo.jpg",
     },
     {
-        nome: "Angela Caroll",
-        ruolo: "Chief Editor",
-        foto: "angela-caroll-chief-editor.jpg",
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        photo: "angela-caroll-chief-editor.jpg",
     },
     {
-        nome: "Walter Gordon",
-        ruolo: "Office Manager",
-        foto: "walter-gordon-office-manager.jpg",
+        name: "Walter Gordon",
+        role: "Office Manager",
+        photo: "walter-gordon-office-manager.jpg",
     },
     {
-        nome: "Angela Lopez",
-        ruolo: "Social Media Manager",
-        foto: "angela-lopez-social-media-manager.jpg",
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        photo: "angela-lopez-social-media-manager.jpg",
     },
     {
-        nome: "Scott Estrada",
-        ruolo: "Developer",
-        foto: "scott-estrada-developer.jpg",
+        name: "Scott Estrada",
+        role: "Developer",
+        photo: "scott-estrada-developer.jpg",
     },
     {
-        nome: "Barbara Ramos",
-        ruolo: "Graphic Designer",
-        foto: "barbara-ramos-graphic-designer.jpg",
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        photo: "barbara-ramos-graphic-designer.jpg",
     },
 ];
 
 //console.log(membersOfTeam);
 
-for(let i = 0; i < membersOfTeam.length; i++) {
+// Stampo i membri del team
+for (let i = 0; i < membersOfTeam.length; i++) {
     // console.log(membersOfTeam[i].nome);
     // console.log(membersOfTeam[i].ruolo);
     // console.log(membersOfTeam[i].foto);
-
+    
     //console.log(`Il ${i+1}° membro del team è ${membersOfTeam[i].nome}, lavora nel ruolo di ${membersOfTeam[i].ruolo} e questa è una sua foto: ${membersOfTeam[i].foto}`);
 
-    boxTeam.innerHTML += `<p>Il ${i+1}° membro del team è ${membersOfTeam[i].nome}, lavora nel ruolo di ${membersOfTeam[i].ruolo} e questa è una sua foto: ${membersOfTeam[i].foto}</p>`;
-}
+    const memberName = document.createElement("h3");
+    memberName.textContent = membersOfTeam[i].name;
 
+    const memberRole = document.createElement("p");
+    memberRole.textContent = membersOfTeam[i].role;
+
+    const memberPhoto = document.createElement("img");
+    memberPhoto.src = `img/${membersOfTeam[i].photo}`;
+    memberPhoto.alt = membersOfTeam[i].photo;
+
+    boxTeam.append(memberName);
+    boxTeam.append(memberRole);
+    boxTeam.append(memberPhoto);
+}
